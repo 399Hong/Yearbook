19756f244f2af94c4c1c73aba06c1a853461e50b
sy <sy@sys-MacBook-Air.hub>
2022-03-10 10:38:48 +1300

    refactored sidebar

---
+++ frontend/yearbook/src/components/sidebar/Sidebar.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Sidebar } from './Sidebar';
+
+
+export default {
+    title: 'Components/Sidebar',
+    component: Sidebar
+
+} as ComponentMeta<typeof Sidebar>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />;
+
+
+export const BasicSidebar = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+// BasicSidebar.args = {
+//     i
+// };
---
+++ frontend/yearbook/src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+    Divider,
+    Link,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    makeStyles,
+} from "@mui/material";
+import ListItemButton from '@mui/material/ListItemButton';
+import HomeIcon from '@mui/icons-material/Home';
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import LoginIcon from '@mui/icons-material/Login';
+import { Box } from "@mui/system"
+
+export const Sidebar = () => {
+
+    return (
+        <Box sx={{ width: 250 }}>
+            <List>
+                {[
+                    [<HomeIcon />, "Home"],
+                    [<ArrowUpwardIcon />, "Submit"],
+                    [<LoginIcon />, "Login"]
+
+                ].map((icon) =>
+
+                    <ListItemButton key = {icon[1]} >
+                        {/* key = {icon[1]} gives an error ? bug? */}
+                        <ListItemIcon>
+                            {/* use to align icon properly  */}
+                            {icon[0]}
+                        </ListItemIcon>
+                        <ListItemText primary={icon[1]} />
+                    </ListItemButton>
+
+                )};
+            </List>
+        </Box >
+
+
+
+
+    );
+
+
+
+};
